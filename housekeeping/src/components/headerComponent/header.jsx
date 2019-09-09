ba0b5f040db4f2186fe96e875ab63534b5b615f8
flychicken123 <flychicken1991@gmail.com>
2019-09-09 00:44:36 -0500

    build homepage

---
+++ housekeeping/src/components/headerComponent/header.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">LOGO</div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Log In</a>
+            </li>
+            <li>
+              <a href="#">Sign Up</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+export default Header;
