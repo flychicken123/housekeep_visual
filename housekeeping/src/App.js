de4d4efbadd51c1685af80961650db81bd9fcc18
flychicken123 <flychicken1991@gmail.com>
2019-09-10 19:37:00 -0500

    fix

--- housekeeping/src/App.js
+++ housekeeping/src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import Header from "./components/headerComponent/header";
 import Footer from "./components/footerComponent/footer";
 import Homepage from "./components/pages/homepage";
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Homepage />
-      <Footer />
-    </div>
-  );
+import "./Assets/css/default.min.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Homepage />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- housekeeping/src/components/pages/homepage.jsx
+++ housekeeping/src/components/pages/homepage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 class Homepage extends Component {
   render() {
-    return <div className="wrap">Home page content</div>;
+    return <div className="container-fluid">Home page content</div>;
   }
 }
 export default Homepage;
