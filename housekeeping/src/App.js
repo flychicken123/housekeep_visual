7d2d54551c2b2807327f6d4f059a4913fb2fa933
flychicken123 <flychicken1991@gmail.com>
2019-09-15 15:19:15 -0500

    account system work

--- housekeeping/src/App.js
+++ housekeeping/src/App.js
@@ -3,7 +3,9 @@ import logo from "./logo.svg";
 import Header from "./components/headerComponent/header";
 import Footer from "./components/footerComponent/footer";
 import Homepage from "./components/pages/homepage";
-import Login from "./components/pages/login";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/Register";
+import Profile from "./components/auth/profile";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./Assets/css/default.min.css";
 class App extends Component {
@@ -14,6 +16,8 @@ class App extends Component {
           <Header />
           <Route exact path="/" component={Homepage} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/sign" component={SignUp} />
+          <Route exact path="/profile" component={Profile} />
           <Footer />
         </div>
       </Router>
