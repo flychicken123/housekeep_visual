749d5eca9cde4fc5df3d480ec9e52d83a6ba2637
flychicken123 <flychicken1991@gmail.com>
2019-09-18 00:24:56 -0500

    update order page

--- housekeeping/src/components/pages/order.jsx
+++ housekeeping/src/components/pages/order.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import "../../Assets/css/order.min.css";
 class Order extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: "",
+      zipCode: ""
+    };
+  }
+  handleAddressChange = event => {
+    this.setState({
+      address: event.target.value
+    });
+  };
   render() {
     return (
       <body>
         <div className="orderAction">
-          <div className="register_form"></div>
+          <div className="register_form">
+            <form className="form">
+              <div className="form-group">
+                <label>Address:</label>
+                <input
+                  type="text"
+                  placeholder="Address"
+                  value={this.state.address}
+                  onChange={this.handleAddressChange}
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </body>
     );
