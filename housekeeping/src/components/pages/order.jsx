63052b4400483c8d3d8e7c77bbf0592078f40384
flychicken123 <flychicken1991@gmail.com>
2019-09-18 23:53:25 -0500

    fix input field position

--- housekeeping/src/components/pages/order.jsx
+++ housekeeping/src/components/pages/order.jsx
@@ -13,19 +13,34 @@ class Order extends Component {
       address: event.target.value
     });
   };
+  handlezipCodeChange = event => {
+    this.setState({
+      zipCode: event.target.value
+    });
+  };
   render() {
     return (
       <body>
         <div className="orderAction">
-          <div className="register_form">
-            <form className="form">
+          <div className="form-1">
+            <form>
+              <h2>Cleaning Job Address</h2>
               <div className="form-group">
-                <label>Address:</label>
+                <label for="address">Address:</label>
                 <input
                   type="text"
                   placeholder="Address"
                   value={this.state.address}
                   onChange={this.handleAddressChange}
+                />
+              </div>
+              <div className="form-group">
+                <label for="zipCode">Zip Code:</label>
+                <input
+                  type="text"
+                  placeholder="Zip Code"
+                  value={this.state.zipCode}
+                  onChange={this.handlezipCodeChange}
                 />
               </div>
             </form>
